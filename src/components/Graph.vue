aed184be928d9f686bfd3c91829193402b484a0f
kyosheek <31596992+kyosheek@users.noreply.github.com>
2020-10-15 17:33:53 +0300

    Try to draw chart using chart.js

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,16 +1,60 @@
 <template>
-  <p>There will be a graph!</p>
+  <canvas id="chart"></canvas>
 </template>
 
 <script>
+import Chart from 'chart.js';
+import data from '@/assets/data';
+
+const drawGraph = () => {
+  const myLabels = [];
+  const myData = [];
+
+  for (let i = 0; i < data.length; i += 1) {
+    myLabels.push(data[i].date);
+    myData.push(data[i].visits);
+  }
+
+  const ctx = document.getElementById('chart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: myLabels,
+      datasets: [{
+        label: '# of Visits',
+        data: myData,
+        backgroundColor: 'rgba(255, 00, 46, 0.5)',
+        borderColor: 'rgba(0, 0, 0, 0.3)',
+        borderWidth: 1,
+      }],
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          },
+        }],
+      },
+      responsive: true,
+      fill: true,
+      pointRadius: 10,
+      pointHoverRadius: 100,
+    },
+  });
+  console.log(chart);
+};
+
 export default {
   name: 'Graph',
   props: {
     data: Array,
   },
+  mounted() {
+    drawGraph();
+  },
 };
 </script>
 
 <style scoped>
-
 </style>
