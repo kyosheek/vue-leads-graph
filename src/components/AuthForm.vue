942c43b68cea915be279dd6646d376a204286366
kyosheek <31596992+kyosheek@users.noreply.github.com>
2020-10-15 16:10:24 +0300

    Add auth page to boilerplate project

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ title }}</h1>
+  <form>
+    <label for="id">ID сайта:<input id="id" type="text"></label>
+    <button>Войти</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AuthForm',
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
